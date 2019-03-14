09232e80193520c7f2b9e2e44121e7fef7b5d14a
Emil Hammer <emil629b@edu.easj.dk>
2019-03-14 15:49:04 +0100

    Add project files.

---
+++ Registreringsafgift/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registreringsafgift
+{
+    public class Class1
+    {
+    }
+}

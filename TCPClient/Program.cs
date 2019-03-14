a3e452a47877142cc3da53a994c8cfdf3e7c6576
Emil Hammer <emil629b@edu.easj.dk>
2019-03-14 22:36:41 +0100

    Første version

---
+++ TCPClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StartListener();
+        }
+
+        private static void StartListener()
+        {
+            try
+            {
+                Thread clientThread = new Thread(Connect);
+                clientThread.Start();
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                for (int i = 5; i > 0; i--)
+                {
+                    Console.WriteLine($"Kan ikke oprette forbindelse til serveren. Prøver igen om {i} sekunder.");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+                StartListener();
+            }
+        }
+
+        private static void Connect()
+        {
+            Console.WriteLine("Opretter forbindelse til server ...");
+            TcpClient client = new TcpClient("127.0.0.1", 7000);
+            Listen(client);
+        }
+
+        private static void Listen(TcpClient client)
+        {
+            NetworkStream nwStream = client.GetStream();
+            StreamReader stReader = new StreamReader(nwStream);
+            StreamWriter stWriter = new StreamWriter(nwStream) { AutoFlush = true };
+
+            Console.WriteLine("Klient forbundet til server!");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(stReader.ReadLine());
+                    stWriter.WriteLine(Console.ReadLine());
+                    Console.Clear();
+                }
+                catch (Exception e)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestRegistringsAfgift/UnitTestAfgift.cs
@@ -0,0 +1,80 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Registreringsafgift;
+
+namespace UnitTestRegistringsAfgift
+{
+    [TestClass]
+    public class UnitTestAfgift
+    {
+        [TestMethod]
+        public void TestBilAfgift()
+        {
+            // Arrange
+            Afgift afgiftKlasse = new Afgift();
+
+            // Act
+            int resultat = afgiftKlasse.BilAfgift(100000);
+            int forventetResultat = 85000;
+
+            // Assert
+            Assert.AreEqual(forventetResultat, resultat);
+        }
+
+        [TestMethod]
+        public void TestBilAfgiftForhøjet()
+        {
+            // Arrange
+            Afgift afgiftKlasse = new Afgift();
+
+            // Act
+            int resultat = afgiftKlasse.BilAfgift(200001);
+            int forventetResultat = 170002;
+
+            // Assert
+            Assert.AreEqual(forventetResultat, resultat);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBilAfgiftException()
+        {
+            // Arrange
+            Afgift afgiftKlasse = new Afgift();
+
+            // Act
+            int resultat = afgiftKlasse.BilAfgift(-1);
+
+            // Assert. Hvis den når hertil skal den fejle da den ikke har smidt en exception som den skal.
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestElBilAfgift()
+        {
+            // Arrange
+            Afgift afgiftKlasse = new Afgift();
+
+            // Act
+            int resultat = afgiftKlasse.ElBilAfgift(100000);
+            int forventetResultat = 17000;
+
+            // Assert
+            Assert.AreEqual(forventetResultat, resultat);
+        }
+
+        [TestMethod]
+        public void TestEilBilAfgiftForhøjet()
+        {
+            // Arrange
+            Afgift afgiftKlasse = new Afgift();
+
+            // Act
+            int resultat = afgiftKlasse.ElBilAfgift(200001);
+            int forventetResultat = 34000;
+
+            // Assert
+            Assert.AreEqual(forventetResultat, resultat);
+        }
+    }
+}
